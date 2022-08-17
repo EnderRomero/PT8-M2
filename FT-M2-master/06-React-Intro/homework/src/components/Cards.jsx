aa89aa4e330226ac271b4316a4b9b374c3e8b990
Ender José Romero <endjr1976@gmail.com>
2022-08-17 00:10:04 -0300

    React intro homework

--- FT-M2-master/06-React-Intro/homework/src/components/Cards.jsx
+++ FT-M2-master/06-React-Intro/homework/src/components/Cards.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import Card from './Card';
+import cities from '../data';
 
 export default function Cards(props) {
   // acá va tu código
   // tip, podés usar un map
-  return <div>Cards Component</div>
+  return <div>
+    {
+    cities.map((city, i) => <Card name={city.name} min={city.main.temp_min} max={city.main.temp_max} img={city.weather[0].icon} onClick={city.onClick} key={i} />)
+    }
+  </div>
 };
